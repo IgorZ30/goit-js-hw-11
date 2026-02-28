b06f9dc27f0df03abcf63d06786d73ca8a897aa4
Igor_Zarubin <iiggooorrrr2@gmail.com>
2026-02-28 04:30:33 +0200

    rename file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import {getImagesByQuery} from "./js/pixabay-api.js";
-import {createGallery} from "./js/render-function.js";
-import {clearGallery} from "./js/render-function.js";
-import {showLoader} from "./js/render-function.js";
-import {hideLoader} from "./js/render-function.js";
+import {createGallery} from "./js/render-functions.js";
+import {clearGallery} from "./js/render-functions.js";
+import {showLoader} from "./js/render-functions.js";
+import {hideLoader} from "./js/render-functions.js";
 
 const form = document.querySelector(".form")
 
